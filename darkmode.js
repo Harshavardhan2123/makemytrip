0c87e6fcd5c394fdfc41b821d3af7b8d4ecf5ef3
Harshavardhan2123 <Harshavardhan2123@gmail.com>
2022-06-24 17:51:27 +0530

    project

---
+++ darkmode.js
@@ -0,0 +1,23 @@
+function changemode()
+{
+    let mybody=document.body;
+    let font=document.getElementById("offers");
+    let dark=document.getElementById("booksection")
+    mybody.classList.toggle("mydark")
+    font.classList.toggle("mydark")
+    dark.classList.toggle("mydark")
+}
+
+
+function loadcoupon()
+{
+    document.getElementById('coupon').style.visibility='visible'
+}
+
+function closecoupon()
+{
+    let coup=document.getElementsByClassName("closecoupon")
+    document.getElementById('coupon').style.visibility='hidden'
+    coup.classList.toggle("closecoupon")
+
+}
